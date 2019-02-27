848cb4198ac7dab801b9978848826360e4f7ed8a
kimhaagenmathiesen <kim_mathiesen@msn.com>
2019-02-27 11:37:44 +0100

    second commit

---
+++ kode1.js
@@ -0,0 +1,9 @@
+function init()
+{
+    for (let i = 0; i < 10; i++) {
+        document.getElementById("lag1").innerHTML = "muhahahahaha";
+        window.alert("muhahahaha");
+    }
+
+}
+document.addEventListener("DOMContentLoaded", init, false)
