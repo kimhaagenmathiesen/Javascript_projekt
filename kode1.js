bc3647cf9dd719c4bc05d5b32df38a0b9c7a5264
kimhaagenmathiesen <kim_mathiesen@msn.com>
2019-03-04 18:01:27 +0100

    new

--- kode1.js
+++
@@ -1,9 +0,0 @@
-function init()
-{
-    for (let i = 0; i < 10; i++) {
-        document.getElementById("lag1").innerHTML = "muhahahahaha";
-        window.alert("muhahahaha");
-    }
-
-}
-document.addEventListener("DOMContentLoaded", init, false)
---
+++ valuta.js
@@ -0,0 +1,9 @@
+
+function klik(){
+    var input = document.getElementById('kroner').value;
+    if(isNaN(input)){
+        window.alert("Værdien skal være et tal!")
+    }
+    var euro = input*7.45;
+    document.getElementById('panel').innerHTML = input + " kroner er = " + euro + " Euro"; 
+}
