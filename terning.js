58bc6d66e173eae3c90102bbb2ae1ea09bf397ed
kimhaagenmathiesen <kim_mathiesen@msn.com>
2019-03-05 10:04:59 +0100

    tirsdag2

---
+++ terning.js
@@ -0,0 +1,7 @@
+function tern() {
+    do{
+        var tal = Math.floor((Math.random()*6)+1);
+        document.getElementById("lag1").innerHTML += "computeren slog " + tal + " ";
+    }
+    while(tal!==6);
+}
