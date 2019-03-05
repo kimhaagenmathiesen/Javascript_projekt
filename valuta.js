594ab61bae979305f34aa50bb8542e46a062538c
kimhaagenmathiesen <kim_mathiesen@msn.com>
2019-03-05 08:52:55 +0100

    tirsdag

--- valuta.js
+++ valuta.js
@@ -1,8 +1,16 @@
 function klik(){
-    var input = document.getElementById('kroner').value;
+    var input = document.getElementById('euro').value;
     if(isNaN(input)){
         window.alert("Værdien skal være et tal!")
     }
     var euro = input*7.45;
-    document.getElementById('panel').innerHTML = input + " kroner er = " + euro + " Euro"; 
+    document.getElementById('panel').innerHTML = input + " euro er = " + euro + " kroner"; 
+}
+function klik2(){
+    var input = document.getElementById('kroner').value;
+    if(isNaN(input)){
+        window.alert("Værdien skal være et tal!")
+    }
+    var euro = input/7.45;
+    document.getElementById('panel2').innerHTML = input + " kroner er = " + euro + "  euro"; 
 }
